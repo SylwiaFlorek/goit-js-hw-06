4e2515aeff04ed6df3dd82c8310202cf39d17c9b
Sylwia Florek <florekbuziaczek199166@gmail.com>
2022-12-05 19:10:24 +0100

    upload

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,5 +1,7 @@
-const ListCategories = document.querySelectorAll('#categories > li');
-console.log(`Number of categories: ${ListCategories.length} `)
+const ListCategories = 
+document.querySelectorAll('#categories > li');
+
+console.log(`Number of categories: ${ListCategories.length}`)
 const categoryElementsCount = ListCategories.forEach((e)=>
-{console.log(`Category: ${e.querySelector('h2').textContent}\nElements: 
+{console.log(`Category:${e.querySelector('h2').textContent}\nElements: 
 ${e.querySelector('ul').childElementCount}`)})
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listIngredients = document.querySelector("ul");
+console.log(listIngredients);
+ingredients.forEach(function(ingredient, index) {
+  ingredient = document.createElement("li");
+  ingredient.textContent = ingredients[index];
+  ingredient.classList.add("item");
+  listIngredients.appendChild(ingredient);
+  console.log(ingredient);
+});
